28643711e92ba0163332266237a16ab2b76b54ae
SamuelRicardos <samuelric4rdo@gmail.com>
2025-04-25 12:41:36 -0300

    ajustes no mobile do index

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,29 @@
-import "./styles.css"
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import "./styles.css";
 
 export default function Header() {
-    return (
-        <header className="header-container">
-            <h1 className="header-title">UnyProducts</h1>
-            <nav>
-                <ul className="header-menu">
-                    <li>Produtos</li>
-                    <li>Quem somos</li>
-                    <li>Sair</li>
-                </ul>
-            </nav>
-        </header>
-    );
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="header-container">
+      <h1 className="header-title">UnyProducts</h1>
+
+      <button
+        className="menu-icon"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Abrir menu"
+      >
+        <FaBars size={22} />
+      </button>
+
+      <nav className={`header-nav ${menuOpen ? "show" : ""}`}>
+        <ul className="header-menu">
+          <li>Produtos</li>
+          <li>Quem somos</li>
+          <li>Sair</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
